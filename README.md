9792bc2e5594289118ab1fffdfca592dd3fa395f
TheCruZ <thecruzwtf@gmail.com>
2024-10-09 23:53:23 +0200

    First

---
+++ README.md
@@ -0,0 +1,9 @@
+# SigMaker-x64
+
+IDA SigMaker Plugin updated for the IDA Pro 9.0
+
+Originally made by P4TR!CK
+
+# Installation
+
+Visual Studio will expect the environment variable IDASDK to resolve to your IDA 9.0 SDK.
---
+++ SigMaker/Includes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#define __IDP__                 1
+#define __NT__                  1
+#define __X64__                 1
+
+#include <ida.hpp>
+#include <idp.hpp>
+#include <expr.hpp>
+#include <bytes.hpp>
+#include <loader.hpp>
+#include <kernwin.hpp>
+#include <diskio.hpp>
+#include <search.hpp>
